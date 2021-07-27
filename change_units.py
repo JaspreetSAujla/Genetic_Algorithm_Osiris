6705c9335cdb7c78ab8cb9c651ff8cd812692089
Clos3y <Clos3y@users.noreply.github.com>
2021-07-27 21:39:51 +0000

    autopep8 action fixes

--- change_units.py
+++ change_units.py
@@ -7,49 +7,50 @@ Takes an input file of system parameters, and converts them to usable relative n
 
 # Bring data in
 
-with open("inputs.json","r") as read_file:
+with open("inputs.json", "r") as read_file:
     data = json.load(read_file)
 
 # Creates dictionaries from the data
 
 for meta_config in data:
 
-	dummy = str(meta_config)
+    dummy = str(meta_config)
 
-	# This creates the dictionaries, so any undefined errors are resolved by this
+    # This creates the dictionaries, so any undefined errors are resolved by
+    # this
 
-	exec(dummy + " = " + str(data[str(meta_config)]))
+    exec(dummy + " = " + str(data[str(meta_config)]))
 
 # Constants
 
-## Taken from CODATA
+# Taken from CODATA
 
-e = 1.602176634e-19 # C, elementary charge
-c = 299792458 # m/s, speed of light in vacuo
-eps0 = 8.8541878128e-12 # F/m, vacuum electric permittivity
-mu0 = 1.25663706212e-6 # N/(A*A). vacuum magnetic permeability
-m_e = 9.1093837015e-31 # kg, electron mass
+e = 1.602176634e-19  # C, elementary charge
+c = 299792458  # m/s, speed of light in vacuo
+eps0 = 8.8541878128e-12  # F/m, vacuum electric permittivity
+mu0 = 1.25663706212e-6  # N/(A*A). vacuum magnetic permeability
+m_e = 9.1093837015e-31  # kg, electron mass
 
 # Calculates the SI values
 
-## Plasma
+# Plasma
 
-uniform_length = Plasma['total_length']  - (sum(Plasma['ramp_lengths']))
-frequency = math.sqrt( (Plasma['density']*e*e) / (eps0 * m_e))
+uniform_length = Plasma['total_length'] - (sum(Plasma['ramp_lengths']))
+frequency = math.sqrt((Plasma['density'] * e * e) / (eps0 * m_e))
 wavelength = 2 * math.pi * c / frequency
-period = wavelength /  c
+period = wavelength / c
 skin_depth = c / frequency
 
-## Laser
+# Laser
 
 frequency = 2 * math.pi * c / Laser['wavelength']
 
-## Witness Beam
+# Witness Beam
 
 num_electrons = Witness_Beam['charge'] / e
 
 
-## System
+# System
 
 x_step = Laser['wavelength'] / 20
 y_step = skin_depth / 4
